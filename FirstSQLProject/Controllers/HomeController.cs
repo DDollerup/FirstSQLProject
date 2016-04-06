cf885920cdace3c4904eaf796c9c52f29961789d
PDDaniel <daniel@proper-dog.com>
2016-04-06 11:19:05 +0200

    Initial Commit

---
+++ FirstSQLProject/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstSQLProject.Factory;
+using FirstSQLProject.Models.BaseModel;
+
+namespace FirstSQLProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        MembersFac membersFac = new MembersFac();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Members> allMembers = membersFac.GetAll();
+            return View(allMembers);
+        }
+    }
+}
---
+++ FirstSQLProject/Models/BaseModel/Members.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstSQLProject.Models.BaseModel
+{
+    public class Members
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string ProfileImage { get; set; }
+        public int GuildID { get; set; }
+    }
+}
